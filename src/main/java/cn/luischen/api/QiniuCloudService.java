28a210d0f5c6ed14ebdb9c08751f019aab3a95aa
yangjie <yangjie@trusfort.com>
2020-11-10 16:29:10 +0800

    .fix:初步调试并继集成七牛云

--- src/main/java/cn/luischen/api/QiniuCloudService.java
+++ src/main/java/cn/luischen/api/QiniuCloudService.java
@@ -41,7 +41,15 @@ public class QiniuCloudService {
     public String upload(MultipartFile file, String fileName) {
 
         //构造一个带指定Zone对象的配置类
-        Configuration cfg = new Configuration(Zone.zone0());
+        /**
+         *
+         * Zone.zone2() 此处需要更新  指的当前空间属于什么地区
+         *
+         *
+         *
+         */
+        //TODO  此处修改为配置类的方式
+        Configuration cfg = new Configuration(Zone.zone2());
         //...其他参数参考类注释
         UploadManager uploadManager = new UploadManager(cfg);
         //默认不指定key的情况下，以文件内容的hash值作为文件名
